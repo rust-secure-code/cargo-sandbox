2e1ba2b29fe2126038e9ebaae9baed7fa11e9466
Tony Arcieri <bascule@gmail.com>
2019-05-18 08:35:35 -0700

    Initial boilerplate

---
+++ src/bin/cargo-sandbox/main.rs
@@ -0,0 +1,15 @@
+//! cargo-sandbox command-line utility
+
+#![deny(
+    warnings,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![forbid(unsafe_code)]
+
+fn main() {
+    cargo_sandbox::start();
+}
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//! cargo sandbox: perform Cargo builds inside of a sandboxed environment
+
+#![forbid(unsafe_code)]
+#![deny(
+    warnings,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![doc(
+    html_logo_url = "https://avatars3.githubusercontent.com/u/44121472",
+    html_root_url = "https://docs.rs/cargo-sandbox/0.0.0"
+)]
+
+/// Start the utility
+pub fn start() {
+    println!("cargo sandbox: perform Cargo builds inside of a sandboxed environment");
+    println!();
+    println!("WORK IN PROGRESS: This utility does not yet provide any functionality");
+    println!("If you are interested in contributing, please see the GitHub issues:");
+    println!();
+    println!("    https://github.com/rust-secure-code/cargo-sandbox/issues");
+    println!();
+}
